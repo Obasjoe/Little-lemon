6d5e818ff12bde27f6861d6a600084571f0afe46
Obasuyi Joseph <Obasjoseph21@gmail.com>
2023-10-07 00:13:01 -0500

    Creating Nav and updating dependencies.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Nav from './Components/Nav';
+
 
 function App() {
   return (
-    <div className="App">
-      Home
-    </div>
+    <>
+      <Nav/>
+    </>
   );
 }
 
---
+++ src/Components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./Nav.css"
+
+export default function Nav() {
+  return (
+    <div>
+        <div className='navigation'>
+            <ul>
+                <li>HOME</li>
+                <li>ABOUT</li>
+                <li>MENU</li>
+                <li>RESERVATIONS</li>
+                <li>ORDER</li>
+                <li>LOGIN</li>
+            </ul>
+        </div>
+    </div>
+  )
+}
